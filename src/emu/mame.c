47515169e83becae0f3fc70c66f12ced244506b8
Bryan Ischo <bryan@ischo.com>
2010-05-12 21:50:48 -0700

    Fixed compiler warning with newer compiler.

--- src/emu/mame.c
+++ src/emu/mame.c
@@ -859,7 +859,7 @@ UINT8 *memory_region(running_machine *machine, const char *name)
 UINT32 memory_region_length(running_machine *machine, const char *name)
 {
 	const region_info *region = machine->region(name);
-	return (region != NULL) ? region->length : NULL;
+	return (region != NULL) ? region->length : 0;
 }
 
 
@@ -871,7 +871,7 @@ UINT32 memory_region_length(running_machine *machine, const char *name)
 UINT32 memory_region_flags(running_machine *machine, const char *name)
 {
 	const region_info *region = machine->region(name);
-	return (region != NULL) ? region->flags : NULL;
+	return (region != NULL) ? region->flags : 0;
 }
 
 
